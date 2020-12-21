09a272a77714f578e482cecb850d866eab86b4cc
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2020-12-21 20:07:34 +0100

    Startseite fertig

--- site/js/client.js
+++ site/js/client.js
@@ -1,1 +1,11 @@
-$("header").load("partials/menu.html");
+$("#alex").click(() => {
+    window.location.href = "alex/projects.html";
+});
+
+$("#korbi").click(() => {
+    window.location.href = "korbi/projects.html";
+});
+
+$("#marc").click(() => {
+    window.location.href = "marc/projects.html";
+});
