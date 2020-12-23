2fd641fa9cce1cea789dfbeb5f3adb00ec6eed62
Korpsian <korbinian.maag@gmail.com>
2020-12-23 12:36:35 +0100

    About me Page + Buggy JS?
    - Ändern von Icon namen
    - aboutme.html fertig
    - menu.js angepasst um bei click event auf die about me page zu gelangen (hat nicht funktioniert bis jetzt)

--- site/js/menu.js
+++ site/js/menu.js
@@ -2,3 +2,16 @@ $(document).ready(() => {
     $("#siteName").text(siteName);
     $("#projectDude").text(projectDude);
 });
+
+$("#about").click(() => {
+    console.log("Clicked on About Me");
+    if(projectDude == "Alexander Baur") {
+        window.location.href = "alex/aboutme.html";
+    }
+    if(projectDude == "Marc Zinser") {
+        window.location.href = "marc/aboutme.html";
+    }
+    if(projectDude == "Korbinian Maag") {
+        window.location.href = "korbi/aboutme.html";
+    }
+});
