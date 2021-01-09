a8cba328add2d637673e7da9d8436a9a32642068
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-01-09 12:27:46 +0100

    Kreise der Slideshow animiert und klickbar gemacht

--- site/js/client.js
+++ site/js/client.js
@@ -13,10 +13,30 @@ $("#marc").click(() => {
 // Slideshow
 let currentImageIndex = 0;
 let offset = 100;
+let circleOffsetPx = 18 + 8;
+let initialCircleOffset = null;
+
+$(window).on("load", () => {
+    setTimeout(() => {
+        let circleAmount = $(".slideShowCircle").length;
+        initialCircleOffset = (circleAmount / 2) * circleOffsetPx - (circleOffsetPx + 3) / 2; // +3 deshalb, weil der aktive Kreis einen 3px größeren radius hat
+        $(".slideShowCircle").css("transform", "translateX(" + (initialCircleOffset) + "px)");
+        $(".slideShowCircle").eq(currentImageIndex).addClass("activeCircle");
+
+        $(".slideShowCircle").each(function(i) { //arrowfunction funktioniert hier aus irgendeinem grund nicht
+            $(this).click(() => {
+                setActiveImage(i);
+            });
+        });
+    }, 100);
+});
 
 function setActiveImage(imageIndex) {
+    $(".slideShowCircle").eq(currentImageIndex).removeClass("activeCircle");
     currentImageIndex = imageIndex;
     $(".projectBackgroundImage").css("transform", "translateX(" + -(offset * currentImageIndex) + "%)");
+    $(".slideShowCircle").css("transform", "translateX(" + (initialCircleOffset - (circleOffsetPx * currentImageIndex)) + "px)");
+    $(".slideShowCircle").eq(currentImageIndex).addClass("activeCircle");
     // let images = document.getElementsByClassName("projectBackgroundImage");
     // for (let x = 0; x < images.length; x++) {
     //     $(images[x]).css("transform", "translateX(" + -(offset * currentImageIndex) + "%)");
