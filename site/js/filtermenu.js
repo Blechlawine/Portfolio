8d4b2e985f4b3fa36b0d551b29e23f481e8b3752
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-01-09 15:05:36 +0100

    Funktionalität Filtermenü fertig

---
+++ site/js/filtermenu.js
@@ -0,0 +1,30 @@
+const highlightOffset = 24 + 24;
+
+function moveHighlight(elementToHighlightID) {
+    let clickedPosition = $("#" + elementToHighlightID).position().top;
+    let calculatedHighlightPosition = clickedPosition - 8 - 6;
+    $("#filterHighlight").css("margin-top", calculatedHighlightPosition + "px");
+}
+
+$(".filterButton").click(function() { //arrowfunction funktioniert hier nicht
+    let placeToJumpTo = this.id.replace("FilterButton", "Category");
+    window.location.href = "#" + placeToJumpTo;
+    moveHighlight(this.id);
+});
+
+$(window).scroll(() => {
+    let currentScrollPos = $(window).scrollTop();
+    let closestCategoryID = null;
+    let closestDistance = null;
+    $(".projectCategory").each(function() {
+        let tempTopOffset = $(this).offset().top;
+        let currentDistance = Math.abs(tempTopOffset - currentScrollPos);
+        if(closestDistance == null || closestDistance > currentDistance) {
+            closestDistance = currentDistance;
+            closestCategoryID = this.id;
+        }
+    });
+    let closestCategoryButtonID = closestCategoryID.replace("Category", "FilterButton");
+    moveHighlight(closestCategoryButtonID);
+    // console.log(closestDistance, closestCategoryButtonID);
+});
